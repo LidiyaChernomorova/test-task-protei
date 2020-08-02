03e156959119cae1b847f7ba529bff169e4e9751
Lidiyacher <chernomorova.lidiya@gmail.com>
2020-08-02 11:52:57 +0300

    TS correct

--- src/app/interfaces/point.model.ts
+++ src/app/interfaces/point.model.ts
@@ -1,11 +1,11 @@
 export interface Point {
     name: string;
     id: number;
-    lat: string;
-    lng: string;
+    lat: number;
+    lng: number;
 }
 
 export interface LatLng {
-    lat: string;
-    lng: string;
+    lat: number;
+    lng: number;
 }
